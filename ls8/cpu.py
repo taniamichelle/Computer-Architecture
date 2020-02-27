f5b269211c96e25c28a62b7c532e7fd5c779b2df
tania <tanislavens@yahoo.com>
2020-02-26 21:24:51 -0500

    call method malfunctioning

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,7 +9,9 @@ instruction_hash = {
     0b01000111: 'PRN',  # Print numeric value stored in the given register. Print to the console the decimal integer value that is stored in the given register
     0b10100010: 'MUL',  # Multipy the values in 2 reg together + store in Reg A
     0b01000101: 'PUSH',  # Push the value in given register onto top of stack
-    0b01000110: 'POP',  # Pop the value at the top of stack into given register
+    0b01000110: 'POP',  # Pop the value at the top of stack into given register, 
+    0b01010000: 'CALL',  # Calls a subroutine at the address stored in the reg
+    0b00010001: 'RET'  # Return from subroutine
 }
 
 class CPU:
@@ -30,40 +32,70 @@ class CPU:
             'HLT': self.execute_hlt,
             'MUL': self.execute_mul,
             'PUSH': self.execute_push,
-            'POP': self.execute_pop
+            'POP': self.execute_pop,
+            'CALL': self.execute_call,
+            'RET': self.execute_ret
         }
     
+    def execute_call(self):
+        # Get address of next instruction
+        reg = self.pc + 2
+        # Push address of next instruction onto top of stack
+        self.ram[self.sp] = reg
+        # Decrement SP
+        self.sp -= 1
+        print("Self.pc 1: ", self.pc)
+        # Set PC to the address stored in the given register (which causes us to jump to that location in RAM)
+        print("Self.ram 1: ", self.ram[self.pc])
+        print("Self.pc 2: ", self.pc + 1)
+        print("Self.ram: ", self.ram[self.pc + 1])
+        self.pc = self.register[self.ram[self.pc + 1]]        
+        print("Self.pc: ", self.pc)
+        
+        print("Register: ", self.register)
+        # First instruction in the subroutine executes.
+        
+    def execute_ret(self):
+        # Increment SP
+        self.sp += 1
+        # Pop the value from the top of the stack 
+        val = self.ram[self.sp]
+        # Store value(address) in the PC
+        self.pc = val
+
     def execute_push(self):
         '''
-        Runs `PUSH`. Stack begins at address F3 and grows downward. SP points at value at top of stack or at F4 if stack is empty.
+        Runs `PUSH`. Stack begins at address F3 and grows downward. SP points at value at top of stack or at F4 if stack is empty. Registers R0-R6 get pushed onto the stack in that order.
         '''
         # Grab register from reg argument
         reg = self.ram[self.pc + 1]
         val = self.register[reg]
-        print("reg in push: ", self.ram[self.pc + 1])
-        print("val in push: ", self.register[reg])
-        print("sp start in push: ", self.sp)
+        # print("reg in push: ", self.ram[self.pc + 1])
+        # print("val in push: ", self.register[reg])
+        # print("sp start in push: ", self.sp)
         # Decrement SP
         self.register[self.sp] -= 1
-        print("sp end in push: ", self.sp)
+        # print("sp end in push: ", self.sp)
         # Copy the value in the given register(from R0-R6) to the address pointed to by SP
         self.ram[self.register[self.sp]] = val
-        self.pc += 2
+        # self.pc += 2
 
     def execute_pop(self):
-        '''Runs `POP`.'''
-        # Grab the value from memory at the top of stack
+        '''
+        Runs `POP`. Registers R6-R0 are popped off the stack in that order.
+        '''
+        #  Grabs the value from memory at the top of stack.
         reg = self.ram[self.pc + 1]
         val = self.ram[self.register[self.sp]]
-        print("reg in pop: ", self.ram[self.pc + 1])
-        print("val in pop: ", self.register[reg])
-        print("sp start in pop: ", self.sp)
+        # print("reg in pop: ", self.ram[self.pc + 1])
+        # print("val in pop: ", self.register[reg])
+        # print("sp start in pop: ", self.sp)
         # Copy the value from address pointed to by SP to the given register
         self.register[reg] = val
         # Increment SP
         self.register[self.sp] += 1
-        print("sp end in pop: ", self.sp)
-        self.pc += 2
+        # print("sp end in pop: ", self.sp)
+        # self.pc += 2
 
     def execute_mul(self):
         '''
@@ -85,7 +117,7 @@ class CPU:
         '''
         Prints numeric value (decimal integer)stored in the given register to console.
         '''
-        print("Operand:", self.register[self.operand_a])
+        print("PRN: ", self.register[self.operand_a])
         # self.pc += self.operands
 
     def execute_hlt(self):
@@ -181,7 +213,7 @@ class CPU:
             elif self.operands == 2:  # If 2 arguments
                 self.operand_a = self.ram_read(self.pc+1)
                 self.operand_b = self.ram_read(self.pc+2)
-            # print("IR", IR)
+            print("IR", IR)
             self.methods_hash[instruction_hash[IR]]()  # Invoke our methods_hash as a function
             self.handle_pc(IR)
             # self.pc += 1  # Increment pc by 1
